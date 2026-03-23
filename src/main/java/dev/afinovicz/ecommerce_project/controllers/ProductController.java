2f9a639b1f0eb6334c46c8274d375736673dab57
AfinoviczDev <jpafinovicz@gmail.com>
2026-03-23 16:42:30 -0300

    feat: criação do endpoint POST para criação de um novo Product

--- src/main/java/dev/afinovicz/ecommerce_project/controllers/ProductController.java
+++ src/main/java/dev/afinovicz/ecommerce_project/controllers/ProductController.java
@@ -5,10 +5,7 @@ import dev.afinovicz.ecommerce_project.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +25,11 @@ public class ProductController {
     @GetMapping
     public Page<ProductDTO> findAll(Pageable pageable) {
         return productService.findAll(pageable);
+    }
+
+    @PostMapping("/")
+    public ProductDTO create(@RequestBody ProductDTO dto) {
+        return productService.insert(dto);
     }
 
 }
