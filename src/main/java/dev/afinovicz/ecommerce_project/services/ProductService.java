c0226e61d0b6d413153fe2d8bbe2c9499463f3f1
AfinoviczDev <jpafinovicz@gmail.com>
2026-03-23 16:31:17 -0300

    feat: criação do endpoint GET para listar todos os produtos de forma listada

--- src/main/java/dev/afinovicz/ecommerce_project/services/ProductService.java
+++ src/main/java/dev/afinovicz/ecommerce_project/services/ProductService.java
@@ -4,9 +4,12 @@ import dev.afinovicz.ecommerce_project.dto.ProductDTO;
 import dev.afinovicz.ecommerce_project.entities.Product;
 import dev.afinovicz.ecommerce_project.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,6 +24,12 @@ public class ProductService {
         Product product = result.get();
         ProductDTO productDTO = new ProductDTO(product);
         return productDTO;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 
 }
